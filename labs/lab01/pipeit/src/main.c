969246b8d845afa947ee9396791f42d085b2353c
JimmyChin946 <Judes946@gmail.com>
2025-09-19 15:04:54 -0700

    testing without child1 clean

--- labs/lab01/pipeit/src/main.c
+++ labs/lab01/pipeit/src/main.c
@@ -1,57 +1,120 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdio.h>
+#include <string.h>
+
+// parent creates pipe ptoc1
+// parent creates pipe c1top2
+// parent creates pipe c2toout
+
+// parent forks child child1
+// parent executes '$ls' command
+// parent sends through pipe ptoc1
+
+// child1 forks child2
+// child1 reads ptoc1
+// child1 executes '$sort -r' command on the ptoc1 data
+// child1 sends through to pipe c1toc2
+
+// child2 (creates no more children)
+// child2 reads c1toc2
+// child2 creates a new file called output
+// child2 sends through to output
+
+// make sure to close all pipes, (even in error)
+// make sure to close all file descriptors (even in error)
+
+
+// small example: skip child1
+
 
 int main(int argc, char *argv[]) {
-	// TODO: change the standardout to a particular file or array or point in memory
-	// output of the commands
-	// char[]* pipedOutput;
+	// forking
 	pid_t pid = 0;
 	int wait_status = 0;
 
+	// file descriptors
+	// int ptoc1[2]; // file desc. for parent -> child1
+	// int c1toc2[2]; // file desc. for child1 -> child2
+	// int c2toout[2]; // file desc. for child2 -> output (a file)
 
+	int fd[2]; // file descriptor for testing
+	if (pipe(fd) == -1) {
+		return EXIT_FAILURE;
+	}
 
-	// create child ONE
+	// output file
+
+	// create child2
 	pid = fork();
 	if (pid == -1) { 
-		return 1; // exit failure
+		return EXIT_FAILURE;
 	}
 
-	// child ONE will execute the 'ls' command
-	if (pid == 0) {
-		// execute the ls command
-		execvp("ls", NULL);
-		return 1; // exit failure
+	if (pid == 0) { // child2 code
+		// do whatever you want in the child code
+		// Close "write end" file descriptor (OR OTHER FILE DESCRIPTORS THAT ARE IRRELEVENT)
+		close(fd[1]);
+
+		// Convert "read end" file descriptor to file stream
+		FILE *f = fdopen(fd[0], "r");
+		if (f == NULL) {
+			close(fd[0]);
+			return EXIT_FAILURE;
+		}
+
+		// Get information from the parent
+		char *buffer = NULL;
+		size_t n = 0;
+		FILE *output_fd = fopen("output.txt", "+w");
+		if (output_fd == NULL) {
+			// printf("something shitty happened");
+			return EXIT_FAILURE;
+		}
+
+		while ((n = getline(&buffer, &n, f)) != -1) {
+			// TODO write to output.txt
+			// printf("%s", buffer);
+			if (fwrite(buffer, sizeof(char), n, output_fd) == -1) {
+				fclose(f);
+				fclose(output_fd);
+				return EXIT_FAILURE;
+			}
+		}
+
+		// Cleanup
+		free(buffer);
+		fclose(output_fd);
+		close(fd[0]);
+		fclose(f);
+
+		return EXIT_SUCCESS;
 	}
+	else { // parent code
+		close(fd[0]); // close the read end of the file descriptor
 
-	waitpid(pid, &wait_status, 0);
+		FILE *f = fdopen(fd[1], "w");
+		if (f == NULL) {
+			close(fd[1]);
+			return EXIT_FAILURE;
+		}
 
-	// break if something went wrong with child 1
-	if (!WIFEXITED(wait_status)) { 
-		return 1; // exit failure
+		char message[] = "this message was sent from the parent to the child (the child wrote it to output.txt)\n";
+		fprintf(f, "%s", message);
+
+		// wait for the child process to finish
+		waitpid(pid, &wait_status, 0);
+
+		if (!WIFEXITED(wait_status)) {
+			return EXIT_FAILURE;
+		}
+
+		close(fd[1]);
+		fclose(f);
+
+		printf("program finished successfully...\n");
+		return EXIT_SUCCESS;
 	}
-
-	// create child TWO
-	pid = fork();
-	if (pid == -1) { 
-		return 1; // exit failure
-	}
-
-	// child TWO will execute the 'ls' command
-	if (pid == 0) {
-		execvp("ls", NULL);
-		return 1; // exit failure
-	}
-
-	waitpid(pid, &wait_status, 0);
-
-	// break if something went wrong with child 1
-	if (!WIFEXITED(wait_status)) { 
-		return 1; 
-	}
-
-	printf("program finished successfully...\n");
-	return 0;
 }
