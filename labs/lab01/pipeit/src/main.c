74aa9aff6de1e25a8504152f14d8309db297ae5c
JimmyChin946 <Judes946@gmail.com>
2025-09-19 22:33:59 -0700

    finished

--- labs/lab01/pipeit/src/main.c
+++ labs/lab01/pipeit/src/main.c
@@ -9,100 +9,138 @@
 #define BUFFER_SIZE 2048
 #define OUT_PERMS 0644 // permissions for the output file
 
-// parent creates pipe ptoc1
-// parent creates pipe c1top2
-// parent creates pipe c2toout
-
-// parent forks child child1
-// parent executes '$ls' command
-// parent sends through pipe ptoc1
-
-// child1 forks child2
-// child1 reads ptoc1
-// child1 executes '$sort -r' command on the ptoc1 data
-// child1 sends through to pipe c1toc2
-
-// child2 (creates no more children)
-// child2 reads c1toc2
-// child2 creates a new file called output
-// child2 sends through to output
-
-// make sure to close all pipes, (even in error)
-// make sure to close all file descriptors (even in error)
-
-
 int main(int argc, char *argv[]) {
 	// forking
 	pid_t pid = 0;
-	int wait_status = 0;
 
-	// file descriptors
-	int parent_child1_fd[2]; // file desc. for parent -> child1
+	// file desc. for parent -> child1
+	int parent_child1_fd[2]; 
 	if (pipe(parent_child1_fd) == -1) {
 		perror("[parent] error making pipe parent_child1_fd");
 		return EXIT_FAILURE;
 	}
-	// int c1toc2[2]; // file desc. for child1 -> child2
 
-
-	// create child2
+	// create child1
 	pid = fork();
 	if (pid == -1) { 
 		close(parent_child1_fd[0]);
 		close(parent_child1_fd[1]);
-		perror("[parent] error forking child2");
+		perror("[parent] error forking child1");
 		return EXIT_FAILURE;
 	}
 
-	if (pid == 0) { // child2 code
-		close(parent_child1_fd[1]);
-
-		// Get information from the parent
-		char buffer[BUFFER_SIZE];
-		ssize_t n = 0;
-
-		int output_fd = open("output", O_CREAT|O_WRONLY|O_TRUNC, OUT_PERMS); 
-		if (output_fd == -1) {
-			close(parent_child1_fd[0]);
-			perror("[child2] could not open output file");
+	if (pid == 0) {
+		// file desc. for child1 -> child2
+		int child1_child2_fd[2]; 
+		if (pipe(child1_child2_fd) == -1) {
+			perror("[child1] error making pipe child1_child2_fd");
 			return EXIT_FAILURE;
-
+		}
+		
+		// create child2
+		pid = fork();
+		if (pid == -1) { 
+			close(parent_child1_fd[0]);
+			close(parent_child1_fd[1]);
+			close(child1_child2_fd[0]);
+			close(child1_child2_fd[1]);
+			perror("[child1] error forking child2");
+			return EXIT_FAILURE;
 		}
 
-		while ((n = read(parent_child1_fd[0], buffer, BUFFER_SIZE)) > 0) {
-			if (write(output_fd, buffer, n) == -1) {
+		if (pid == 0)	{ // child2 code
+			// we don't need to access pipe parent_child1_fd as child2
+			close(parent_child1_fd[0]);
+			close(parent_child1_fd[1]);
+
+			// keep the "read side" of child1_child2_fd
+			// (therefore, close the "write side" of that pipe
+			close(child1_child2_fd[1]);
+
+			printf("THIS IS CHILD2!!!");
+
+			char buffer[BUFFER_SIZE];
+			ssize_t n = 0;
+
+			int output_fd = open("output", O_CREAT|O_WRONLY|O_TRUNC, OUT_PERMS); 
+			if (output_fd == -1) {
 				close(parent_child1_fd[0]);
+				perror("[child2] could not open output file");
+				return EXIT_FAILURE;
+			}
+
+			while ((n = read(child1_child2_fd[0], buffer, BUFFER_SIZE)) > 0) {
+				if (write(output_fd, buffer, n) == -1) {
+					close(child1_child2_fd[0]);
+					close(output_fd);
+					perror("[child2] error writing to output file");
+					return EXIT_FAILURE;
+				}
+			}
+
+			if (n == -1) {
+				close(child1_child2_fd[0]);
 				close(output_fd);
-				perror("[child2] error writing to output file");
+				perror("[child2] error reading from pipe parent_child1_fd");
+				return EXIT_FAILURE;
+			}
+
+			printf("THIS IS CHILD2!!!");
+
+			close(child1_child2_fd[1]);
+			return EXIT_SUCCESS;
+		}
+		// TODO: get rid of the else statements?
+		else { // child1 code
+			// keep the "read side" of parent_child1_fd
+			// (therefore, close the "write side" of that pipe)
+			close(parent_child1_fd[1]);
+
+			// keep the "write side" of child1_child2_fd
+			// (therefore, close the "read side" of that pipe)
+			close(child1_child2_fd[0]);
+			
+			// change stdin (to be from the parent_child1_fd "read side")
+			if (dup2(parent_child1_fd[0], STDIN_FILENO) == -1) {
+				close(parent_child1_fd[0]);
+				close(child1_child2_fd[1]);
+				return EXIT_FAILURE;
+			}
+			// change stdout (to be to the child1_child2_fd "write side")
+			if (dup2(child1_child2_fd[1], STDOUT_FILENO) == -1) {
+				close(parent_child1_fd[0]);
+				close(child1_child2_fd[1]);
+				return EXIT_FAILURE;
+			}
+
+			close(parent_child1_fd[0]);
+			close(child1_child2_fd[1]);
+
+			// execute stdin
+			if (execlp("sort", "sort", "-r", NULL) == -1) {
+				perror("[child1] error executing \"$sort -r\" command");
 				return EXIT_FAILURE;
 			}
 		}
-
-		if (n == -1) {
-			close(parent_child1_fd[0]);
-			close(output_fd);
-			perror("[child2] error reading from pipe parent_child1_fd");
-			return EXIT_FAILURE;
-		}
-
-		close(parent_child1_fd[0]);
-		close(output_fd);
-
-		return EXIT_SUCCESS;
 	}
+	// TODO: get rid of the else statements?
 	else { // parent code
 		close(parent_child1_fd[0]);
-
+		
+		// redirect stdout to the file descriptor that is the "write" end of 
+		// the parent to child 1 pipe
 		if (dup2(parent_child1_fd[1], STDOUT_FILENO) == -1) {
 			close(parent_child1_fd[1]); 
 			return EXIT_FAILURE;
 		}
 		close(parent_child1_fd[1]); 
 
-
+		// the output which goes to stdout is now the write end of the pipe
+		// when ls is executed, the output will flow through that pipe
 		if (execlp("ls", "ls", NULL) == -1) {
-			perror("[parent] error execlp ls command");
+			perror("[parent] error executing \"$ls\" command");
 			return EXIT_FAILURE;
 		}
 	}
 }
+
