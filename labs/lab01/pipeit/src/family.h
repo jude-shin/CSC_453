4b95ab4d24e8d8f6814e57cf61fc16efb7124a61
JimmyChin946 <Judes946@gmail.com>
2025-09-20 16:52:07 -0700

    remove child2 from main file

--- labs/lab01/pipeit/src/family.h
+++ labs/lab01/pipeit/src/family.h
@@ -1,3 +1,2 @@
-void parent(int *parent_child1_fd);
-void child1(int *parent_child1_fd, int *child1_child2_fd);
-void child2(int *child1_child2_fd);
+void parent(int *fd);
+void child1(int *fd);
--- labs/lab01/pipeit/src/main.c
+++ labs/lab01/pipeit/src/main.c
@@ -6,8 +6,13 @@
 
 #include "family.h"
 
+// TODO: ASK Ask ask if I should close the write end here (asap), or
+// in the function (as it is now... for readablity)
+// for both the child1 and parent functions
+
+
+
 int main(int argc, char *argv[]) {
-  // hellllo this is new 
   // forking
   pid_t pid = 0;
 
@@ -28,50 +33,15 @@ int main(int argc, char *argv[]) {
   }
 
   if (pid == 0) {
-    // file desc. for child1 -> child2 pipe 
-    int child1_child2_fd[2]; 
-    if (pipe(child1_child2_fd) == -1) {
-      perror("[child1] error making pipe child1_child2_fd");
-      return EXIT_FAILURE;
-    }
-
-    // create a new process (child2) as child1
-    pid = fork();
-    if (pid == -1) { 
-      close(parent_child1_fd[0]);
-      close(parent_child1_fd[1]);
-      close(child1_child2_fd[0]);
-      close(child1_child2_fd[1]);
-      perror("[child1] error forking child2");
-      return EXIT_FAILURE;
-    }
-    
-    if (pid == 0)	{ // if you are child2:
-      // we don't need to access pipe parent_child1_fd as child2
-      close(parent_child1_fd[0]);
-      close(parent_child1_fd[1]);
-
-      // TODO: ASK Ask ask if I should close the write end here (asap), or
-      // in the function (as it is now... for readablity)
-
-      // redirect stdout to a new file descriptor for the output file
-      // take stream and write to the output fd using a buffer
-      child2(child1_child2_fd);
-    }
-    
     // perform child1 actions
     // take stdin from parent_child1_fd pipe that was written by parent
-    // redirect stdout to child1_child2_fd pipe for child2 to read
-    // execute '$ sort -r' command
-    child1(parent_child1_fd, child1_child2_fd);
+    // redirect stdout to output_fd pipe for the ouput file 
+    // execute '$ sort -r' command, which writes the stream to the 
+    child1(parent_child1_fd);
   }
-  
-  // TODO: ASK Ask ask if I should close the write end here (asap), or
-  // in the function (as it is now... for readablity)
 
   // perform parent actions
   // redirect stdout to parent_child1_fd pipe for child1 to read
   // execute '$ ls' command,
   parent(parent_child1_fd);
 }
-
