1362f4b71673782178b629d6fba678cd9532526f
JimmyChin946 <Judes946@gmail.com>
2025-09-21 00:06:45 -0700

    small

--- labs/lab01/pipeit/src/family.c
+++ labs/lab01/pipeit/src/family.c
@@ -3,8 +3,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define BUFFER_SIZE 2048
-#define OUT_PERMS 0644 // permissions for the output file
+#define BUFFER_SIZE 2048 // write size for buffers writing to files
+#define OUT_PERMS 0644 // permissions for the 'outfile'
 
 void child2(int *ipc_fd) {
   // The "read side" of the ipc_fd pipe will be used, so close the unused 
@@ -12,22 +12,22 @@ void child2(int *ipc_fd) {
   close(ipc_fd[1]);
 
   // Make the outfile_fd file descriptor and open as writable, creating the
-  // file if it does not exsist. Give general permissions to it as well.
+  // file if it does not exsist. Give general permissions to the "outfile".
   int outfile_fd = open("outfile", O_CREAT|O_WRONLY|O_TRUNC, OUT_PERMS); 
   if (outfile_fd == -1) {
     close(ipc_fd[0]);
-    perror("[child2] could not open output file");
+    perror("[child2] error opening oufile");
     exit(EXIT_FAILURE);
   }
 
   // Change stdin to be the "read side" of the ipc_fd pipe so the "sort"  
-  // command uses the output from child1's exec of the "$ ls" command.
+  // command uses the output from child1's exec of the "ls" command.
   if (dup2(ipc_fd[0], STDIN_FILENO) == -1) {
     close(ipc_fd[0]);
     exit(EXIT_FAILURE);
   }
 
-  // Change stdout to be the outfile_fd so that the "$ sort -r" command outputs
+  // Change stdout to be the outfile_fd so that the "sort -r" command writes 
   // to the 'outfile'.
   if (dup2(outfile_fd, STDOUT_FILENO) == -1) {
     close(ipc_fd[0]);
@@ -41,7 +41,7 @@ void child2(int *ipc_fd) {
   // Note: execlp will only return on failure, and will send the appropriate 
   // exit(EXIT_FAILURE) and exit(EXIT_SUCCESS) signals to the parent.
   execlp("sort", "sort", "-r", NULL);
-  perror("[child2] error executing \"$sort -r\" command");
+  perror("[child2] error executing \"sort -r\" command");
   exit(EXIT_FAILURE);
 }
 
@@ -50,8 +50,8 @@ void child1(int *ipc_fd) {
   // "read side" of that pipe.
   close(ipc_fd[0]);
 
-  // Change stdout to be the "write side" of the ipc_fd pipe so that the "$ ls"
-  // command outputs to the ipc_fd pipe that will be used by child2.
+  // Change stdout to be the "write side" of the ipc_fd pipe so that the "ls"
+  // command writes to the ipc_fd pipe that will be used by child2.
   if (dup2(ipc_fd[1], STDOUT_FILENO) == -1) {
     close(ipc_fd[1]); 
     exit(EXIT_FAILURE);
@@ -62,6 +62,6 @@ void child1(int *ipc_fd) {
   // Note: execlp will only return on failure, and will send the appropriate 
   // exit(EXIT_FAILURE) and exit(EXIT_SUCCESS) signals to the parent.
   execlp("ls", "ls", NULL);
-  perror("[child1] error executing \"$ls\" command");
+  perror("[child1] error executing \"ls\" command");
   exit(EXIT_FAILURE);
 }
--- labs/lab01/pipeit/src/main.c
+++ labs/lab01/pipeit/src/main.c
@@ -19,12 +19,14 @@
 // TODO: Should I use the _ or camel case?
 
 int main(int argc, char *argv[]) {
-  // forking variables
+  // forking variables for child1 and child2
   int wait_status_c1, wait_status_c2;
   pid_t pid_c1, pid_c2 = 0;
-
-  // setup file descriptors (a pipe) for c1 <-> c2 processes
+  
+  // interprocess communication file descriptors (for the pipe)
   int ipc_fd[2]; 
+
+  // setup the pipe for c1 <-> c2 processes
   if (pipe(ipc_fd) == -1) {
     perror("[parent] error making pipe ipc_fd");
     exit(EXIT_FAILURE);
@@ -46,6 +48,7 @@ int main(int argc, char *argv[]) {
     // toss that output through the pipe ipc_fd for child 2 to recieve
     child1(ipc_fd);
   }
+  // ==========================================
 
   // create the child 2 process 
   pid_c2 = fork();
@@ -59,8 +62,9 @@ int main(int argc, char *argv[]) {
   // ==========================================
   // perform child 2 logic 
   if (pid_c2 == 0) {
-    // executes the '$ sort -r' command on the incoming stream from pipe ipc_fd
-    // and writes it to the file in the project directory called 'output'
+    // executes the '$ sort -r' command on the incoming stream from pipe 
+    // ipc_fd and writes it to the file in the project directory called 
+    // "outfile".
     child2(ipc_fd);
   }
 
@@ -70,9 +74,9 @@ int main(int argc, char *argv[]) {
   close(ipc_fd[0]);
   close(ipc_fd[1]);
 
-  // wait for both processes to finish
-  // if both are successfull, finish the program and return 0
-  // else, something went wrong, so return -1
+  // Wait for both processes to finish. If both are successfull, indicate so
+  // by returning with a zero status. If one or both exit unsuccessfully, 
+  // return a nonzero to indicate something went wrong.
   waitpid(pid_c1, &wait_status_c1, 0);
   waitpid(pid_c2, &wait_status_c2, 0);
 
@@ -80,6 +84,7 @@ int main(int argc, char *argv[]) {
     printf("Program Finished Successfully!\n");
     return 0;
   }
-
-  return -1;
+  
+  // TODO: do we put perrors at the end? or do we just have the program finish?
+  return -1; 
 }
