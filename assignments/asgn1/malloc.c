423f1ec9c05d0a4dd55851a3d8ce47f1fce739ce
JimmyChin946 <Judes946@gmail.com>
2025-09-22 23:33:13 -0700

    ideas

--- assignments/asgn1/malloc.c
+++ assignments/asgn1/malloc.c
@@ -2,7 +2,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define SBRK_BUFFER_SIZE
+
+// NOTE: use multiples of 16 bytes
+
 
 int main(int argc, char *argv[]) {
+  // use sbrk to generate a buffer size
+  // the return should be the beginning pointer where we can start allocating
+
+  // use a linked list style data structure
+
   return 0;
 }
