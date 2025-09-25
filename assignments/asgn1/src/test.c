65b1a7df45b257986d47a7c0626f8daf1332aaac
JimmyChin946 <Judes946@gmail.com>
2025-09-24 20:34:29 -0700

    merging seems to work

--- assignments/asgn1/src/test.c
+++ assignments/asgn1/src/test.c
@@ -1,16 +1,15 @@
 #include "alloc.h"
 
 int main(int argc, char *argv[]) {
-  // break here: get the inital size of the global header
+  void *ptr1 = my_malloc(64);
+  void *ptr2 = my_malloc(32);
+  void *ptr3 = my_malloc(16);
+  // void *ptr4 = my_malloc(16);
 
-  void *ptr = my_malloc(16);
+  my_free(ptr1);
+  my_free(ptr3);
 
-  // break here: get the size of the header
-
-  my_free(ptr);
-
-  // break here: get the size of the header
-  // should be the same as the first time you looked
+  my_free(ptr2);
 
   return 0;
 }
