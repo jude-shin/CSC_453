1660e1429cc9e32964fff7516b8cde51a44ec0dd
JimmyChin946 <Judes946@gmail.com>
2025-09-26 22:55:57 -0700

    hunk size that nico likes

--- assignments/asgn1/src/test.c
+++ assignments/asgn1/src/test.c
@@ -3,11 +3,7 @@
 #include "alloc.h"
 
 int main(int argc, char *argv[]) {
-  char *ptr1 = (char *)my_malloc(16);
-
-  memcpy(ptr1, "1234567812345678", 16);
-
-  char *realloced_ptr = (char *)my_realloc(ptr1, 32); 
+  char *ptr1 = (char *)my_calloc(1, 256);
 
   return 0;
 }
