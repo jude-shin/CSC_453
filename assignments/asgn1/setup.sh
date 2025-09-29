0cf336c57c38305184d447174033df53a6f437d2
Jude Shin <judes946@gmail.com>
2025-09-28 17:11:58 -0700

    setup for switching workspaces

--- assignments/asgn1/setup.sh
+++ assignments/asgn1/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH=//home/jshin53/csc_453/assignments/asgn1/lib64:
+LD_LIBRARY_PATH="//home/jshin53/csc_453/assignments/asgn1/lib64://home/jshin53/csc_453/assignments/asgn1/lib://home/jude/fall_2025/csc_453/assignments/asgn1/lib64://home/jude/fall_2025/csc_453/assignments/asgn1/lib"
 
+
+export LD_LIBRARY_PATH
+export DEBUG_MALLOC=""
