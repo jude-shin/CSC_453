ba13f92a9589a57e1943b7a14e3d37fc1b9159da
Jude Shin <judes946@gmail.com>
2025-09-28 19:30:56 -0700

    I don't know why the 32 won't compile on this system

--- assignments/asgn1/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-LD_LIBRARY_PATH="//home/jshin53/csc_453/assignments/asgn1/lib64://home/jshin53/csc_453/assignments/asgn1/lib://home/jude/fall_2025/csc_453/assignments/asgn1/lib64://home/jude/fall_2025/csc_453/assignments/asgn1/lib"
-
-
-export LD_LIBRARY_PATH
-export DEBUG_MALLOC=""
