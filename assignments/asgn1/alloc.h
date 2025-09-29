96ee2badbc539090f5b5672ebe5b52beae30f888
Jude Shin <judes946@gmail.com>
2025-09-29 16:27:05 -0700

    comments

--- assignments/asgn1/alloc.h
+++ assignments/asgn1/alloc.h
@@ -3,9 +3,14 @@
 
 #include <stddef.h>
 
+// Allocates memory of nmemb*size bytes. Sets everything to 0.
 void *calloc(size_t nmemb, size_t size);
+// Allocates memory of size bytes. Contents are not guarenteed.
 void *malloc(size_t size);
+// De-Allocates previously allocated memory to be used again at ptr.
 void free(void *ptr);
+// Change the size of a previously allocated chunk of memory at ptr to 
+// size bytes.
 void *realloc(void *ptr, size_t size);
 
 #endif
