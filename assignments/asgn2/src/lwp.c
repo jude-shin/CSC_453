78687b19781cc6893e7e4aa1333a5d2db23a4fd3
JimmyChin946 <Judes946@gmail.com>
2025-10-09 12:36:51 -0700

    never copy paste from the latex manual

--- assignments/asgn2/src/lwp.c
+++ assignments/asgn2/src/lwp.c
@@ -5,7 +5,7 @@
 // lwp create() returns the (lightweight) thread id of the new thread
 // or NO THREAD if the thread cannot be created.
 tid_t lwp_create(lwpfun function, void *argument){
-  return NULL;
+  return 0;
 }
 
 
@@ -32,26 +32,30 @@ void lwp_exit(int exitval) {
 // ports its termination status if status is non-NULL.
 // Returns the tid of the terminated thread or NO THREAD.
 tid_t lwp_gettid(void) {
-  return NULL;
+  return 0;
 }
 
 // Returns the tid of the calling LWP or NO THREAD if not called by a
 // LWP.
-thread tid2thread(tid t tid) {
-  return NULL;
+thread tid2thread(tid_t tid) {
+  return 0;
 }
 
 // Returns the thread corresponding to the given thread ID, or NULL
 // if the ID is invalid
 tid_t lwp_wait(int *status) {
-  return NULL;
+  return 0;
 }
 
 // Causes the LWP package to use the given scheduler to choose the
 // next process to run. Transfers all threads from the old scheduler
 // to the new one in next() order. If scheduler is NULL the library
 // should return to round-robin scheduling.
-// scheduler lwp get scheduler(void);
+void lwp_set_scheduler(scheduler sched) {
+
+}
+
 // Returns the pointer to the current scheduler.
-void lwp_get_scheduler(scheduler sched) {
+scheduler lwp_get_scheduler(void) {
+  return (void*)0;
 }
