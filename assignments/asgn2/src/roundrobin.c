c19488edc8b4f9bc29ce6e91eb07568009c839a0
JimmyChin946 <Judes946@gmail.com>
2025-10-11 08:43:00 -0700

    sched ll functions

--- assignments/asgn2/src/roundrobin.c
+++ assignments/asgn2/src/roundrobin.c
@@ -18,7 +18,7 @@ scheduler RoundRobin = {
 thread sched_pool_head = NULL;
 
 // // TODO: add the tail in later
-// thread sched_pool_tail = NULL;
+thread sched_pool_tail = NULL;
 
 // Add the passed context to the scheduler’s scheduling pool.
 // For round robin, this thread is added to the end of the list
@@ -54,3 +54,53 @@ int rr_qlen(void) {
   return 0;
 }
 
+
+// ======================================
+
+// Tack on the new thread to the end of the globally linked list (to the tail)
+// This only handles the scheduler's linked list portion
+void append_sched_ll(thread new) {
+  // TODO: change this in the header file
+  // sched_one is the NEXT pointer
+  // sched_two is the PREV pointer
+
+  // If the tail is NULL, then the head must also be NULL
+  // Set both the head and the tail to the new thread
+  if (sched_pool_tail == NULL) {
+    // Make sure these are pointing to NULL to indicate they are the head and 
+    // tail respectively
+    new->sched_one = NULL;
+    new->sched_two = NULL;
+    
+    sched_pool_head = new;
+    sched_pool_tail = new;
+    return;
+  }
+
+  // new's 'next' becomes cur's 'next'
+  new->sched_one = sched_pool_tail->sched_one;
+  // new's 'prev' becomes cur
+  new->sched_two = sched_pool_tail;
+
+  // cur's 'next' becomes new
+  sched_pool_tail->sched_one = new;
+  // cur's 'prev' stays the same
+  // new->sched_two = new->sched_two;
+}
+
+// Removes a victim from the lined list
+// This only handles the scheduler's linked list portion
+void remove_sched_ll(thread victim) {
+  // TODO: change this in the header file
+  // sched_one is the NEXT pointer
+  // sched_two is the PREV pointer
+
+  // (victim's prev)'s next becomes (victim's next)
+  if (victim->sched_two != NULL) {
+    victim->sched_two->sched_one = victim->sched_one;
+  }
+  // (victim's next)'s prev becomes (victim's prev)
+  if (victim->sched_one != NULL) {
+    victim->sched_one->sched_two = victim->sched_two;
+  }
+}
