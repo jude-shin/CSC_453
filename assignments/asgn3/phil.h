f7c319d4953474e7ea2b9989d71279849192e6a1
JimmyChin946 <Judes946@gmail.com>
2025-10-19 13:57:40 -0700

    circular linked list

--- assignments/asgn3/phil.h
+++ assignments/asgn3/phil.h
@@ -1,11 +1,10 @@
 
 char get_label(int i);
 
-typedef struct PhilContext {
-  // Ascii value of this philosopher's name
-  char name;
-  // The philosopher can either be thinking, eating, or changing
-  int doing;
-  // The philosopher currently wants
-  int wants;
+typedef struct Phil {
+  char name;   // Ascii value of this philosopher's name
+  int doing;   // What the philosopher is doing (thinking, eating, or changing)
+  int wants;   // What the philosopher currently wants (thinky, hungry)
+  Phil *right; // The philosopher sitting to the right
+  Phil *left;  // The philosopher sitting to the left 
 } Phil;
