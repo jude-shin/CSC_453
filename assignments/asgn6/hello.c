b17a39b24e736e3e00bff6e7a46e29bcc70f03cb
JimmyChin946 <Judes946@gmail.com>
2025-12-05 10:20:27 -0800

    check syscalls

--- assignments/asgn6/hello.c
+++ assignments/asgn6/hello.c
@@ -4,7 +4,11 @@
 
 int main(int argc, char *argv[]) {
   /* Greet the world by printing "Hello, world!" followed by a newline. */
-  printf("Hello, world!\n");
+  /* If this syscall fails, it returns a value less than 0. */
+  if (printf("Hello, world!\n") < 0) {
+    perror("error with printf()\n");
+    exit(-1);
+  }
 
   /* Exit with a zero exit status indicating success. */
   exit(0);
